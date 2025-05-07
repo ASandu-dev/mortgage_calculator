91de339d63eab2a1dbc63a7fe07c59377e107c1e
@AS <andrei.sandu@computerminds.co.uk>
2025-05-07 16:32:16 +0100

    fix3

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
-import type { NextConfig } from "next";
+const isGithubPages = process.env.GITHUB_PAGES === 'true';
 
-const nextConfig: NextConfig = {
-  /* config options here */
+const nextConfig = {
+  output: 'export',
+  basePath: isGithubPages ? '/mortgage_calculator' : '',
+  assetPrefix: isGithubPages ? '/mortgage_calculator/' : '',
 };
 
 export default nextConfig;
